4435a4f4721d88cf25953be187dc742008c8f6f9
its-me-ojas <crestfallenwizard@gmail.com>
2023-08-02 10:31:08 +0530

    3

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "1.Create\n"
+            << "2.Display\n"
+            << "3.Insert\n"
+            << "4.Delete\n"
+            << "5.Linear Search"
+            << "6.Exit" << std::endl;
+  int choice;
+  std::cin >> choice;
+  switch (choice) {
+    case 1:
+
+  }
+  return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,14 @@
+// Predict the Output of the following program
+#include <iostream>
+
+int main() {
+  int i;
+  int arr[5] = {1};
+  for (i = 0; i < 5; i++)
+    std::cout << arr[i];
+  return 0;
+}
+
+/* OUTPUT
+ * 10000
+ **/

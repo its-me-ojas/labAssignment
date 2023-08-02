87214e73f9d84b346ac3ad694ec1ed5086ad572f
its-me-ojas <crestfallenwizard@gmail.com>
2023-08-02 11:48:18 +0530

    4c

---
+++ 4c.cpp
@@ -0,0 +1,58 @@
+// Find the Transpose of a Matrix
+
+#include <iostream>
+int main() {
+
+  int rows, cols;
+  std::cout << "Enter the rows and cols";
+  std::cin >> rows >> cols;
+
+  // break case
+  if (rows != cols) {
+    std::cout << "Square matrix needed";
+    return 0;
+  }
+  // allocating memory for the arrays
+  int **arr = new int *[rows];
+  int **trans = new int *[rows];
+  for (size_t i = 0; i < cols; i++) {
+    arr[i] = new int[cols];
+  }
+  for (size_t i = 0; i < cols; i++) {
+    trans[i] = new int[cols];
+  }
+
+  // initialising the arrays
+  std::cout << "Enter the elements" << std::endl;
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      std::cin >> arr[i][j];
+    }
+  }
+
+  // printing the array
+  std::cout << "Original" << std::endl;
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      std::cout << arr[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  // tranposing
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      trans[j][i] = arr[i][j];
+    }
+  }
+
+  // printing the array
+  std::cout << "Transpose" << std::endl;
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      std::cout << trans[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}

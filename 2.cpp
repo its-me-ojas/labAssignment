16f20b58e27c28166f6663d3b163c9cea9fa4bb2
its-me-ojas <crestfallenwizard@gmail.com>
2023-08-08 22:38:03 +0530

    update

--- 2.cpp
+++ 2.cpp
@@ -1,51 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-void display(int arr[], int size) {
-  for (size_t i = 0; i < size; i++)
-    std::cout << arr[i] << " ";
-  std::cout << std::endl;
-}
-
-void deleteElement(int arr[], int index, int &size) {
-  if (index < 0 || index >= size || index == -1) {
-    std::cout << "Cannot delete element from that index" << std::endl;
-    return;
-  }
-  for (size_t i = index; i < size; i++) {
-    arr[i] = arr[i + 1];
-  }
-  size--;
-  std::cout << std::endl;
-  display(arr, size);
-}
-
-int linearSearch(int arr[], int key, int size) {
-  for (size_t i = 0; i < size; i++) {
-    if (arr[i] == key)
-      return i;
-  }
-  return -1;
+std::vector<int> removeDuplicates(const std::vector<int>& arr) {
+    std::vector<int> uniqueElements;
+    for (int element : arr) {
+        if (std::find(uniqueElements.begin(), uniqueElements.end(), element) == uniqueElements.end()) {
+            uniqueElements.push_back(element);
+        }
+    }
+    return uniqueElements;
 }
 
 int main() {
+    std::vector<int> inputArray = {1, 2, 2, 3, 4, 4, 5};
+    std::vector<int> uniqueArray = removeDuplicates(inputArray);
 
-  std::cout << "Enter the size of the array;";
-  int n;
-  std::cin >> n;
-  int *arr = new int[n];
-  std::cout << "Enter the elements in the array" << std::endl;
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> arr[i];
-  }
-  std::cout << "Original array :";
-  for (size_t i = 0; i < n; i++) {
-    std::cout << arr[i] << " ";
-  }
+    for (int element : uniqueArray) {
+        std::cout << element << " ";
+    }
 
-  for (size_t i = 0; i < n; i++) {
-    int key = arr[i];
-    int index = linearSearch(arr + i, key, n - i);
-    deleteElement(arr, index, n);
-  }
-  return 0;
+    return 0;
 }

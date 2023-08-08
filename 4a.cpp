1237710992d1e673fa00ca1c29b82b53d0307f26
its-me-ojas <crestfallenwizard@gmail.com>
2023-08-08 23:54:14 +0530

    concatenate

---
+++ 4a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+int main() {
+  std::string str1, str2;
+  std::cout << "enter string 1 : ";
+  std::getline(std::cin, str1);
+  std::cout << std::endl;
+  std::cout << "enter string 2 : ";
+  std::getline(std::cin, str2);
+  std::string str3 = str1 + str2;
+  std::cout << str3 << std::endl;
+
+  return 0;
+}

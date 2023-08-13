9ed0e3e972a84983683b0db71daddb6351bd2a83
its-me-ojas <crestfallenwizard@gmail.com>
2023-08-13 19:37:13 +0530

    distinct elements

---
+++ 8.cpp
@@ -0,0 +1,26 @@
+// Write a program to count the total number of distinct elements in an array of
+// length n.
+
+#include <iostream>
+#include <unordered_set>
+
+int distinctElements(int arr[], int size) {
+  // we will be using unordered_set because it can store distinct values even if
+  // we add multiple duplicate values
+  std::unordered_set<int> distinct;
+  for (size_t i = 0; i < size; i++) {
+    distinct.insert(arr[i]);
+  }
+  return distinct.size();
+}
+
+int main() {
+
+  int arr[] = {1, 1, 1, 1, 2, 4, 5, 5, 6, 6, 3, 3, 8, 9, 7, 7};
+  int size = sizeof(arr) / sizeof(int);
+
+  std::cout << distinctElements(arr, size) << " distinct elements are present"
+            << std::endl;
+
+  return 0;
+}

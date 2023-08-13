0d195acfcbb5c37ec0357960ed9f5e271b3b64b9
its-me-ojas <crestfallenwizard@gmail.com>
2023-08-13 18:56:38 +0530

    upper to lower

--- 4e.cpp
+++ 4e.cpp
@@ -1,8 +1,10 @@
 // Write a program to convert a character from uppercase to lowercase.
 #include <iostream>
 
-int main() { 
+int main() {
   char c;
-  std::cout<<"enter a character"
-  return 0; 
+  std::cout << "enter a character : ";
+  std::cin >> c;
+  std::cout << "The character in lower case is : " << static_cast<char>(c + 32);
+  return 0;
 }

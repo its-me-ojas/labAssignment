d437ffe8d5529643a57d8505286bdb64fe3d3ac1
its-me-ojas <crestfallenwizard@gmail.com>
2023-09-13 15:24:21 +0530

    Middle of linked list

--- 1.cpp
+++ 1.cpp
@@ -1,115 +1,155 @@
-// Develop a menu driven program demonstrating the following operations on a
-// Stack using array: (i) push(), (ii) pop(), (iii) isEmpty(), (iv) isFull(),
-// (v) display(), and (vi) peek().
+// Develop a menu driven program for the following operations of on a Singly
+// Linked List. (a) Insertion at the beginning. (b) Insertion at the end. (c)
+// Insertion in between (before or after a node having a specific value, say
+// 'Insert a new Node 35 before/after the Node 30'). (d) Deletion from the
+// beginning. (e) Deletion from the end. (f) Deletion of a specific node, say
+// 'Delete Node 60'). (g) Search for a node and display its position from head.
+// (h) Display all the node values
+
+#include <algorithm>
 #include <iostream>
-#include <limits>
 
-void choices() {
-  std::cout << "1. PUSH\n"
-            << "2. POP\n"
-            << "3. isEMPTY\n"
-            << "4. isFULL\n"
-            << "5. DISPLAY\n"
-            << "6. PEEK\n"
-            << "7. EXIT" << std::endl;
+class Node {
+public:
+  int data;
+  Node *next;
+
+  // constructor
+  Node(int data) {
+    this->data = data;
+    this->next = NULL;
+  }
+  // destructor
+  ~Node() {
+    int value = this->data;
+    if (this->next != NULL) {
+      delete next;
+      this->next = NULL;
+    }
+    std::cout << "Memory is free for node with data" << std::endl;
+  }
+};
+
+void insertAtHead(Node *&head, int data) {
+  // new node create
+  Node *temp = new Node(data);
+  temp->next = head;
+  head = temp;
 }
 
-// void clearInputBuffer() {
-//   std::cin.clear();
-//   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-// }
+void insertAtTail(Node *&tail, int data) {
+  Node *temp = new Node(data);
+  tail->next = temp;
+  tail = temp;
+}
 
-void push(int s[], int size, int &top) {
-  if (top == size - 1) {
-    std::cout << "OVERFLOW CONDITION" << std::endl;
+void print(Node *head) {
+  Node *temp = head;
+  while (temp != NULL) {
+    std::cout << temp->data << " ";
+    temp = temp->next;
+  }
+  std::cout << std::endl;
+}
+
+void insertAtPosition(Node *&head, Node *&tail, int position, int data) {
+  // insert at starting case
+  if (position == 1) {
+    insertAtHead(head, data);
     return;
-  } else {
-    top++;
-    std::cout << "Enter the element : " << std::endl;
-    int x;
-    std::cin >> x;
-    s[top] = x;
   }
-}
-
-void pop(int s[], int &top) {
-  if (top == -1) {
-    std::cout << "UNDERFLOW CONDITION" << std::endl;
+  Node *nodeToInsert = new Node(data);
+  Node *temp = head;
+  int cnt = 1;
+  while (cnt < position - 1) {
+    temp = temp->next;
+    cnt++;
+  }
+  // insert at last position
+  if (temp->next == NULL) {
+    insertAtTail(tail, data);
     return;
-  } else {
-    int x = s[top];
-    top--;
-    std::cout << "POPPED : " << x << std::endl;
   }
+  nodeToInsert->next = temp->next;
+  temp->next = nodeToInsert;
 }
 
-void isEmpty(int &top) {
-  if (top == -1) {
-    std::cout << "TRUE" << std::endl;
+void deleteNode(Node *&head, Node *&tail, int position) {
+  // deleting first or last node
+  if (position == 1) {
+    Node *temp = head;
+    head = head->next;
+    // Memory free from node
+    temp->next = NULL;
+    delete temp;
+    // Check if the list is empty
+    if (head == NULL) {
+      tail = NULL;
+    }
   } else {
-    std::cout << "FALSE" << std::endl;
-  }
-}
-
-void isFull(int size, int &top) {
-  if (top == size - 1) {
-    std::cout << "TRUE" << std::endl;
-    ;
-  } else {
-    std::cout << "FALSE" << std::endl;
-  }
-}
-
-void display(int s[], int top) {
-  std::cout << "[";
-  for (int i = top; i >= 0; i--) {
-    std::cout << s[i] << ",";
-  }
-  std::cout << "]" << std::endl;
-}
-
-void peek(int s[], int top) {
-  if (top == -1) {
-    std::cout << "Stack is empty" << std::endl;
-  } else {
-    std::cout << "Element is : " << s[top] << std::endl;
+    // deleting any middle node or last node
+    Node *current = head;
+    Node *previous = NULL;
+    int cnt = 1;
+    while (cnt < position) {
+      previous = current;
+      current = current->next;
+      cnt++;
+    }
+    previous->next = current->next;
+    current->next = NULL;
+    delete current;
+    // Check if the deleted node was the last node
+    if (previous->next == NULL) {
+      tail = previous;
+    }
   }
 }
 
 int main() {
-  int stack[10], size = 10, top = -1;
-  bool loop = true;
-  while (loop) {
-    int choice = 0;
-    choices();
-    std::cin >> choice;
-    // clearInputBuffer();
-    std::fflush(stdin);
-    switch (choice) {
-    case 1:
-      push(stack, size, top);
-      break;
-    case 2:
-      pop(stack, top);
-      break;
-    case 3:
-      isEmpty(top);
-      break;
-    case 4:
-      isFull(size, top);
-      break;
-    case 5:
-      display(stack, top);
-      break;
-    case 6:
-      peek(stack, top);
-      break;
-    case 7:
-      loop = false;
-      break;
-    default:
-      std::cout << "Invalid Option" << std::endl;
-    }
+  int data, position;
+  Node *head = NULL;
+  Node *tail = NULL;
+  std::cout << "1) Insertion at the beginning\n"
+            << "2) Insertion at the end\n"
+            << "3) insertion in between\n"
+            << "4) Deletion from the beginning\n"
+            << "5) Deletion from the end\n"
+            << "6) Deletion of a specific node\n"
+            << "7) Search for a node and display it\n"
+            << "8) Display all the node values\n";
+  int choice;
+  std::cin >> choice;
+  switch (choice) {
+  case 1:
+    std::cout << "What to insert:";
+    std::cin >> data;
+    insertAtHead(head, data);
+    break;
+  case 2:
+    std::cout << "What to insert:";
+    std::cin >> data;
+    insertAtTail(tail, data);
+    break;
+  case 3:
+    insertAtPosition(head, tail, position, data);
+    break;
+  case 4:
+    deleteNode(head, data);
+    break;
+  case 5:
+    deleteNode(head, tail, position);
+    break;
+  case 6:
+    std::cout << "Enter the position:";
+    std::cin >> position;
+    deleteNode(head, tail, position);
+    break;
+  case 7:
+
+    break;
+  case 8:
+    break;
   }
   return 0;
 }
--- 3.cpp
+++ 3.cpp
@@ -1,44 +1,64 @@
-// Write a program that checks if an expression has balanced parentheses.
+// Write a program to find the middle of a linked list.
+// Input: 1->2->3->4->5
+// Output- 3
 
 #include <iostream>
-#include <stack>
-#include <string>
 
-bool areParenthesesBalanced(const std::string &expression) {
-  std::stack<char> parenthesesStack;
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-  for (char ch : expression) {
-    if (ch == '(' || ch == '[' || ch == '{') {
-      parenthesesStack.push(ch);
-    } 
-    else if (ch == ')' || ch == ']' || ch == '}') {
-      if (parenthesesStack.empty()) {
-        return false; // Unbalanced, as there's no matching opening parenthesis
+void createLinkedList(ListNode *&head, int n) {
+  head = nullptr;
+  for (int i = 0; i < n; i++) {
+    ListNode *newNode = new ListNode(i);
+    if (head == nullptr) {
+      head = newNode;
+    } else {
+      ListNode *temp = head;
+      while (temp->next != nullptr) {
+        temp = temp->next;
       }
-
-      char top = parenthesesStack.top();
-      parenthesesStack.pop();
-      if ((ch == ')' && top != '(') || (ch == ']' && top != '[') ||
-          (ch == '}' && top != '{')) {
-        return false; // Mismatched parentheses
-      }
+      temp->next = newNode;
     }
   }
+}
 
-  return parenthesesStack
-      .empty(); // If stack is empty, all parentheses are balanced
+void printLinkedList(ListNode *head) {
+  ListNode *temp = head;
+  while (temp != nullptr) {
+    std::cout << temp->val << " -> ";
+    temp = temp->next;
+  }
+  std::cout << std::endl;
 }
 
 int main() {
-  std::string expression;
-  std::cout << "Enter an expression: ";
-  std::getline(std::cin, expression);
+  ListNode *head;
+  int n;
+  std::cout << "Enter the length of the linked list:";
+  std::cin >> n;
 
-  if (areParenthesesBalanced(expression)) {
-    std::cout << "Parentheses are balanced." << std::endl;
-  } else {
-    std::cout << "Parentheses are not balanced." << std::endl;
+  createLinkedList(head, n);
+  printLinkedList(head);
+
+  if (head == nullptr) {
+    return 0;
   }
 
+  ListNode *slow = head;
+  ListNode *fast = head->next;
+
+  while (fast != nullptr && fast->next != nullptr) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+
+  // slow now points to the middle node
+  std::cout << "The middle element is " << slow->val << std::endl;
   return 0;
 }
